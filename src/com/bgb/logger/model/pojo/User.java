967a24e08e0f78800dcac8db0b86d83db7879e94
bill9361 <fengminbiao@126.com>
2018-07-01 17:24:14 +0800

    USER

--- src/com/bgb/logger/model/pojo/User.java
+++ src/com/bgb/logger/model/pojo/User.java
@@ -27,116 +27,199 @@ public class User extends BaseModel
     private String roleId;
     private String userGroupId;
     
+    private CompanyOrganization companyOrganization;
     
-    
+	
 	public String getUsername() {
 		return username;
 	}
+
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
+
 	public String getPassword() {
 		return password;
 	}
+
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+
 	public String getFullname() {
 		return fullname;
 	}
+
+
 	public void setFullname(String fullname) {
 		this.fullname = fullname;
 	}
+
+
 	public String getIdNumber() {
 		return idNumber;
 	}
+
+
 	public void setIdNumber(String idNumber) {
 		this.idNumber = idNumber;
 	}
+
+
 	public String getDateOfBirth() {
 		return dateOfBirth;
 	}
+
+
 	public void setDateOfBirth(String dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
 	}
+
+
 	public Integer getIsLock() {
 		return isLock;
 	}
-	public void setIsLock(int isLock) {
+
+
+	public void setIsLock(Integer isLock) {
 		this.isLock = isLock;
 	}
+
+
 	public Integer getWorkingStatus() {
 		return workingStatus;
 	}
-	public void setWorkingStatus(int workingStatus) {
+
+
+	public void setWorkingStatus(Integer workingStatus) {
 		this.workingStatus = workingStatus;
 	}
+
+
 	public String getPicture() {
 		return picture;
 	}
+
+
 	public void setPicture(String picture) {
 		this.picture = picture;
 	}
+
+
 	public String getValidityStartDate() {
 		return validityStartDate;
 	}
+
+
 	public void setValidityStartDate(String validityStartDate) {
 		this.validityStartDate = validityStartDate;
 	}
+
+
 	public String getValidityEndDate() {
 		return validityEndDate;
 	}
+
+
 	public void setValidityEndDate(String validityEndDate) {
 		this.validityEndDate = validityEndDate;
 	}
+
+
 	public String getCompanyOrgId() {
 		return companyOrgId;
 	}
+
+
 	public void setCompanyOrgId(String companyOrgId) {
 		this.companyOrgId = companyOrgId;
 	}
+
+
 	public String getPostId() {
 		return postId;
 	}
+
+
 	public void setPostId(String postId) {
 		this.postId = postId;
 	}
+
+
 	public String getTel() {
 		return tel;
 	}
+
+
 	public void setTel(String tel) {
 		this.tel = tel;
 	}
+
+
 	public String getEmail() {
 		return email;
 	}
+
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+
 	public String getQq() {
 		return qq;
 	}
+
+
 	public void setQq(String qq) {
 		this.qq = qq;
 	}
+
+
 	public String getWechat() {
 		return wechat;
 	}
+
+
 	public void setWechat(String wechat) {
 		this.wechat = wechat;
 	}
+
+
 	public String getRoleId() {
 		return roleId;
 	}
+
+
 	public void setRoleId(String roleId) {
 		this.roleId = roleId;
 	}
+
+
 	public String getUserGroupId() {
 		return userGroupId;
 	}
+
+
 	public void setUserGroupId(String userGroupId) {
 		this.userGroupId = userGroupId;
 	}
+
+
+	public CompanyOrganization getCompanyOrganization() {
+		return companyOrganization;
+	}
+
+
+	public void setCompanyOrganization(CompanyOrganization companyOrganization) {
+		this.companyOrganization = companyOrganization;
+	}
+
+
 	@Override
 	public String toString() {
 		return "User [username=" + username + ", password=" + password + ", fullname=" + fullname + ", idNumber="
